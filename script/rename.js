2442d70405eff33ed889319a5bad0699b327d7e3
thy <972143891@qq.com>
2021-06-14 10:42:25 +0800

    feat: add tool

--- script/rename.js
+++ script/rename.js
@@ -4,7 +4,7 @@ var path = require("path");
 // 有 4 个值 serial short single appendix
 var dealPath = '../../static/zhiBuMieDeNi/serial'
 // var dealPath = './test'
-var dealFile = [1] // 测试 需要重命名的文件夹
+var dealFile = [] // 测试 需要重命名的文件夹
 // var dealFile = [11,12,13,14,15,16,17,18,19,20,21,22,23] // 需要重命名的文件夹
 var fileArr = []; // 存储目标文件路径
 
@@ -57,7 +57,41 @@ function readDir(dir) {
   }
 }
 
+function getFileNewName(data,type) {
+  let newName = data
+  let hasChange = false;
+  if (type == 1) {
+    const eleTypePos =  data.lastIndexOf('.')
+    const fileType = ele.substring(eleTypePos)
+    let eleSplitFirst =  ele.split('\\')
+    const posFirst = eleSplitFirst.length-1
+    const eleSplitEle = eleSplitFirst[posFirst]
+    let eleSplit =  eleSplitEle.split('.')
+    const fileName = eleSplit[0]
+    const matchArr = fileName.match(/\d{1,6}/g)
+    const matchData = Number(matchArr[0])
+    const newName = `${matchData}${fileType}`
 
+    eleSplitFirst[posFirst] = newName
+    newName = eleSplitFirst.join('\\')
+  }
+  if (type == 2) {
+    let strSplit =  data.split('\\')
+    const lastIndex = strSplit.length-1
+    const oldNameType = strSplit[lastIndex]
+    let oldNameSplit =  oldNameType.split('.')
+    const fileType = oldNameSplit[oldNameSplit.length-1]
+    const fileName = oldNameSplit[0]
+    if (fileName.indexOf('0') == 0) {
+      hasChange = true
+      const matchArr = fileName.match(/\d{1,6}/g)
+      const oldName = Number(matchArr[0])
+      strSplit[lastIndex] = `${oldName}.${fileType}`
+      newName = strSplit.join('\\')
+    }
+  }
+  return {newName,hasChange}
+}
 
 function renameFile() {
   if(dealFile.length) {
@@ -71,27 +105,13 @@ function renameFile() {
 
   // console.log(fileArr)
   fileArr.map(ele => {
-    // const isNeed = ele.indexOf('_') > -1
-    const isNeed = 'all'
-    if (isNeed) {
-      const eleTypePos =  ele.lastIndexOf('.')
-      const fileType = ele.substring(eleTypePos)
-      let eleSplitFirst =  ele.split('\\')
-      const posFirst = eleSplitFirst.length-1
-      const eleSplitEle = eleSplitFirst[posFirst]
-      let eleSplit =  eleSplitEle.split('.')
-      const fileName = eleSplit[0]
-      const matchArr = fileName.match(/\d{1,6}/g)
-      const matchData = Number(matchArr[0])
-      const newName = `${matchData}${fileType}`
-
-      eleSplitFirst[posFirst] = newName
-      const newPathName = eleSplitFirst.join('\\')
-
-      fs.renameSync(ele, newPathName)
+    const {newName, hasChange} = getFileNewName(ele,2)
+    if (hasChange) {
+      console.log(`rename one done ${ele}`)
+      fs.renameSync(ele, newName)
     }
   })
-  console.log('rename file done')
+  console.log('rename all file done')
 }
 
 
@@ -119,7 +139,7 @@ function getDirectoryNewName(data,order,type) {
 
 function renameDirectory() {
   readDir(dealPath)
-  // console.log(fileArr)
+  console.log(fileArr)
   fileArr.map((ele,index) => {
       let newName = getDirectoryNewName(ele,index,2)
       fs.renameSync(ele, newName)
@@ -128,5 +148,5 @@ function renameDirectory() {
   console.log('rename Directory done')
 }
 
-// renameFile()
-renameDirectory()
+renameFile()
+// renameDirectory()
